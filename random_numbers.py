555f4904520ff3a755c687f11c3a60134d44f807
backs0 <danielbackstroem@hotmail.com>
2025-11-21 14:24:54 +0100

    Add files via upload

---
+++ random_numbers.py
@@ -0,0 +1,22 @@
+import random
+
+# --- Constants ---
+MIN_RANDOM = 0
+MAX_RANDOM = 999
+INVALID_INPUT_MESSAGE = "Invalid Input"
+
+
+
+def main():
+    while True:
+        try:
+            number = input("How many random numbers in the range 0 – 999 are desired? ")
+            
+
+        
+
+
+
+# Run the program
+if __name__ == "__main__":
+    main()
